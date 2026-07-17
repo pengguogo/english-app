83f5ee470063d250e909e73b9fa195ba44fe0d79
dawn <653457162@qq.com>
2026-07-17 17:51:21 +0800

    chore: init Android project with Jetpack Compose, Hilt, Retrofit, Room

---
+++ android/app/src/main/java/com/englishapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.englishapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.englishapp.ui.theme.EnglishAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EnglishAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("Hello English App!")
+                }
+            }
+        }
+    }
+}
