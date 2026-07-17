a565b62d44393e56a57dccda3ed989c33d3073ad
dawn <653457162@qq.com>
2026-07-17 18:15:06 +0800

    feat: add learning map screen with navigation (Task 12)
    - Create MapViewModel with Hilt injection and lesson loading via Retrofit
    - Create MapScreen with TopAppBar and LazyColumn lesson list
    - Update MainActivity with NavHost for home/map navigation

--- android/app/src/main/java/com/englishapp/MainActivity.kt
+++ android/app/src/main/java/com/englishapp/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.englishapp.ui.home.HomeScreen
+import com.englishapp.ui.map.MapScreen
 import com.englishapp.ui.theme.EnglishAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") { HomeScreen() }
+                        composable("map") { MapScreen() }
+                    }
                 }
             }
         }
